1111c2c0c8929b81fee7c20e2be646242763bb68
Caroline Leopold <caroline.leopold@gmail.com>
2019-01-24 13:11:43 -0500

    save before test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,21 @@
-import { GetBacon } from './utils';
+/* import { GetBacon } from './utils';
 const baconEl = document.querySelector('.bacon');
 GetBacon()
   .then(res => {
     const markup = res.reduce((acc, val) => (acc += `<p>${val}</p>`), '');
     baconEl.innerHTML = markup;
   }).catch(err => (baconEl.innerHTML = err));
+
+  */
+var myObj, i, x = "";
+  myObj = {
+  "category":"Medical Editorial Style",
+  "author":"Caroline Leopold",
+  "brands":[ "Benefiber", "Overture", "Colgate" ]
+  };
+
+  for (i in myObj.brands) {
+  x += myObj.brands[i] + "<br>";
+  }
+ 
+  document.getElementById("portfolio-item").innerHTML = x;
