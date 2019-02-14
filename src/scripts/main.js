934f6fc62fe4713987cce36e7c041de3a16b7b3e
Caroline Leopold <caroline.leopold@gmail.com>
2019-02-14 11:08:19 -0500

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,4 @@
-/* import { GetBacon } from './utils';
-const baconEl = document.querySelector('.bacon');
-GetBacon()
-  .then(res => {
-    const markup = res.reduce((acc, val) => (acc += `<p>${val}</p>`), '');
-    baconEl.innerHTML = markup;
-  }).catch(err => (baconEl.innerHTML = err));
 
-  */
 var myObj, i, x = "";
   myObj = {
   "category":"Medical Editorial Style",
